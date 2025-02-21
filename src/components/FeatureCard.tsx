59a5a0ef34ff457123fad9bc2a8abda1e98acedd
Bhaumik Raja <webdevbhaumik@gmail.com>
2025-02-21 13:05:35 -0500

    Working on Features section.

---
+++ src/components/FeatureCard.tsx
@@ -0,0 +1,18 @@
+export default function FeatureCard(props: {
+    title: string;
+    description: string;
+    children?: React.ReactNode;
+}) {
+    const { title, description, children } = props;
+    return (
+        <div>
+            <div>
+                <div>{children}</div>
+                <div>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,6 @@
+import Tag from "@/components/Tag";
+import FeatureCard from "@/components/FeatureCard";
+
 const features = [
     "Asset Library",
     "Code Preview",
@@ -9,5 +12,42 @@ const features = [
 ];
 
 export default function Features() {
-    return <div>Features</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <Tag>Features</Tag>
+                </div>
+                <h2 className="text-white text-6xl font-medium text-center mt-6">
+                    Where power meets{" "}
+                    <span className="text-lime-400">simplicity</span>
+                </h2>
+                <div>
+                    <FeatureCard
+                        title="Real-time Collaboration"
+                        description="Work together seamlessly with conflict-free
+                                    editing."
+                    ></FeatureCard>
+                    <FeatureCard
+                        title="Interactive Prototyping"
+                        description="Engage your clients with prototypes that
+                                    react to user actions."
+                    ></FeatureCard>
+                    <FeatureCard
+                        title="Keyboard Quick Actions"
+                        description="Powerful commands that help you create
+                                    designs more quickly."
+                    ></FeatureCard>
+                </div>
+                <div>
+                    {features.map((feature) => (
+                        <div key={feature}>
+                            <span></span>
+                            <span>{feature}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
