40de784b58a77869c6fa77994cd59f315ad70624
Bhaumik Raja <webdevbhaumik@gmail.com>
2025-02-23 14:44:54 -0500

    Working on Integrations section.

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -1,12 +1,80 @@
+import Tag from "@/components/Tag";
+import figmaIcon from "@/assets/images/figma-logo.svg";
+import notionIcon from "@/assets/images/notion-logo.svg";
+import slackIcon from "@/assets/images/slack-logo.svg";
+import relumeIcon from "@/assets/images/relume-logo.svg";
+import framerIcon from "@/assets/images/framer-logo.svg";
+import githubIcon from "@/assets/images/github-logo.svg";
+import Image from "next/image";
+
 const integrations = [
-    { name: "Figma", icon: "figma-logo.svg", description: "Figma is a collaborative interface design tool." },
-    { name: "Notion", icon: "notion-logo.svg", description: "Notion is an all-in-one workspace for notes and docs." },
-    { name: "Slack", icon: "slack-logo.svg", description: "Slack is a powerful team communication platform." },
-    { name: "Relume", icon: "relume-logo.svg", description: "Relume is a no-code website builder and design system." },
-    { name: "Framer", icon: "framer-logo.svg", description: "Framer is a professional website prototyping tool." },
-    { name: "GitHub", icon: "github-logo.svg", description: "GitHub is the leading platform for code collaboration." },
+    {
+        name: "Figma",
+        icon: figmaIcon,
+        description: "Figma is a collaborative interface design tool.",
+    },
+    {
+        name: "Notion",
+        icon: notionIcon,
+        description: "Notion is an all-in-one workspace for notes and docs.",
+    },
+    {
+        name: "Slack",
+        icon: slackIcon,
+        description: "Slack is a powerful team communication platform.",
+    },
+    {
+        name: "Relume",
+        icon: relumeIcon,
+        description: "Relume is a no-code website builder and design system.",
+    },
+    {
+        name: "Framer",
+        icon: framerIcon,
+        description: "Framer is a professional website prototyping tool.",
+    },
+    {
+        name: "GitHub",
+        icon: githubIcon,
+        description: "GitHub is the leading platform for code collaboration.",
+    },
 ];
 
 export default function Integrations() {
-    return <div>Integrations</div>;
+    return (
+        <section className="py-24 overflow-hidden">
+            <div className="container">
+                <Tag>Integrations</Tag>
+                <h2 className="text-6xl font-medium mt-6 text-white">
+                    Plays well with{" "}
+                    <span className="text-lime-400">others</span>
+                </h2>
+                <p className="text-white/50 mt-4 text-lg">
+                    Layers seamlessly connects with your favorite tools to make
+                    it easy to plug into any workflow and collaborate across
+                    platforms.
+                </p>
+                <div>
+                    {integrations.map((integration) => (
+                        <div
+                            key={integration.name}
+                            className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
+                        >
+                            <div className="flex justify-center">
+                                <Image
+                                    src={integration.icon}
+                                    alt={`${integration.name} icon`}
+                                    className="size-24"
+                                ></Image>
+                            </div>
+                            <h3 className="text-3xl text-center mt-6">
+                                {integration.name}
+                            </h3>
+                            <p>{integration.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
